d2dbdeb56600a26fca2fb4747698b9c4e6193831
Josue Romero <josuepinro21@unitec.edu>
2023-10-25 08:07:16 -0600

    NodemodulesFix

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var mongoose = require("mongoose");
+
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
